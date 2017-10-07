e6c8f4ef9a4d8a492f909eb7e5afd075ba3400f0
Kristiyan <k.koshnicharov@abv.bg>
2017-10-07 20:53:46 +0300

    tag, video, pucture added

--- SportalDBAlpha.sql
+++ SportalDBAlpha.sql
@@ -18,6 +18,121 @@ USE `sportal`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `article`
+--
+
+DROP TABLE IF EXISTS `article`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `article` (
+  `article_id` int(11) NOT NULL,
+  `sub_category` int(11) NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `content` longtext NOT NULL,
+  `date` date NOT NULL,
+  `impressions` int(11) DEFAULT NULL,
+  `status_id` int(11) NOT NULL,
+  `object_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`article_id`),
+  KEY `title` (`title`) USING BTREE,
+  KEY `categori_id_idx` (`sub_category`),
+  KEY `status_id_idx` (`status_id`),
+  KEY `object_id_idx` (`object_id`),
+  KEY `idx_article_title` (`title`),
+  CONSTRAINT `object_id` FOREIGN KEY (`object_id`) REFERENCES `object` (`object_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `status_id` FOREIGN KEY (`status_id`) REFERENCES `status` (`status_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `sub_category` FOREIGN KEY (`sub_category`) REFERENCES `sub_category` (`sub_category_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='longtext check\ncategories table\nnot sure about comments\n/where to add sub_category it has to be from category but not sure\nstatus_id breaking, top, shock:)\nobject_id Levski, CSKA...\n...add subscribers may be\nremoved tag, picture and video because  both refer to article_id';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `article`
+--
+
+LOCK TABLES `article` WRITE;
+/*!40000 ALTER TABLE `article` DISABLE KEYS */;
+/*!40000 ALTER TABLE `article` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `article_pics`
+--
+
+DROP TABLE IF EXISTS `article_pics`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `article_pics` (
+  `article_id` int(11) NOT NULL,
+  `picture_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`article_id`),
+  KEY `pucture_idx` (`picture_id`),
+  CONSTRAINT `article` FOREIGN KEY (`article_id`) REFERENCES `article` (`article_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `pucture` FOREIGN KEY (`picture_id`) REFERENCES `picture` (`picture_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `article_pics`
+--
+
+LOCK TABLES `article_pics` WRITE;
+/*!40000 ALTER TABLE `article_pics` DISABLE KEYS */;
+/*!40000 ALTER TABLE `article_pics` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `article_tags`
+--
+
+DROP TABLE IF EXISTS `article_tags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `article_tags` (
+  `article_id` int(11) NOT NULL,
+  `tag_id` int(11) NOT NULL,
+  PRIMARY KEY (`article_id`,`tag_id`),
+  KEY `tag_idx` (`tag_id`),
+  CONSTRAINT `articleT` FOREIGN KEY (`article_id`) REFERENCES `article` (`article_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `tag` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`tag_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `article_tags`
+--
+
+LOCK TABLES `article_tags` WRITE;
+/*!40000 ALTER TABLE `article_tags` DISABLE KEYS */;
+/*!40000 ALTER TABLE `article_tags` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `article_videos`
+--
+
+DROP TABLE IF EXISTS `article_videos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `article_videos` (
+  `article_id` int(11) NOT NULL,
+  `video_id` int(11) NOT NULL,
+  PRIMARY KEY (`video_id`,`article_id`),
+  KEY `article_id_idx` (`article_id`),
+  CONSTRAINT `articleV_id` FOREIGN KEY (`article_id`) REFERENCES `article` (`article_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `video_id` FOREIGN KEY (`video_id`) REFERENCES `video` (`video_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `article_videos`
+--
+
+LOCK TABLES `article_videos` WRITE;
+/*!40000 ALTER TABLE `article_videos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `article_videos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `categories`
 --
 
@@ -52,16 +167,19 @@ DROP TABLE IF EXISTS `comments`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `comments` (
   `comment_id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `id_news` int(11) NOT NULL,
+  `author` int(11) NOT NULL,
+  `article` int(11) NOT NULL,
   `content` varchar(200) DEFAULT NULL,
+  `answer` int(11) DEFAULT NULL,
   `likes` int(11) DEFAULT NULL,
   `dislikes` int(11) DEFAULT NULL,
   PRIMARY KEY (`comment_id`),
-  KEY `user_id_idx` (`user_id`),
-  KEY `id_news_idx` (`id_news`),
-  CONSTRAINT `id_news` FOREIGN KEY (`id_news`) REFERENCES `news` (`id_news`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `user_id_idx` (`author`),
+  KEY `answer_idx` (`answer`),
+  KEY `article_id_idx` (`article`),
+  CONSTRAINT `answer` FOREIGN KEY (`answer`) REFERENCES `comments` (`comment_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `article_id` FOREIGN KEY (`article`) REFERENCES `article` (`article_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `user_id` FOREIGN KEY (`author`) REFERENCES `users` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -72,44 +190,6 @@ CREATE TABLE `comments` (
 LOCK TABLES `comments` WRITE;
 /*!40000 ALTER TABLE `comments` DISABLE KEYS */;
 /*!40000 ALTER TABLE `comments` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `news`
---
-
-DROP TABLE IF EXISTS `news`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `news` (
-  `id_news` int(11) NOT NULL AUTO_INCREMENT,
-  `category_id` int(11) DEFAULT NULL,
-  `title` varchar(200) NOT NULL,
-  `content` longtext NOT NULL,
-  `media` blob,
-  `date` date NOT NULL,
-  `impressions` int(11) DEFAULT NULL,
-  `tags` varchar(100) DEFAULT NULL,
-  `status_id` int(11) DEFAULT NULL,
-  `object_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_news`),
-  KEY `title` (`title`) USING BTREE,
-  KEY `categori_id_idx` (`category_id`),
-  KEY `status_id_idx` (`status_id`),
-  KEY `object_id_idx` (`object_id`),
-  CONSTRAINT `categori_id` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `object_id` FOREIGN KEY (`object_id`) REFERENCES `object` (`object_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `status_id` FOREIGN KEY (`status_id`) REFERENCES `status` (`status_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='longtext check\ncategories table\nnot sure about comments\n/where to add sub_category it has to be from category but not sure\nstatus_id breaking, top, shock:)\nobject_id Levski, CSKA...\n...add subscribers may be';
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `news`
---
-
-LOCK TABLES `news` WRITE;
-/*!40000 ALTER TABLE `news` DISABLE KEYS */;
-/*!40000 ALTER TABLE `news` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -137,6 +217,30 @@ CREATE TABLE `object` (
 LOCK TABLES `object` WRITE;
 /*!40000 ALTER TABLE `object` DISABLE KEYS */;
 /*!40000 ALTER TABLE `object` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `picture`
+--
+
+DROP TABLE IF EXISTS `picture`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `picture` (
+  `picture_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `content` blob NOT NULL,
+  PRIMARY KEY (`picture_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `picture`
+--
+
+LOCK TABLES `picture` WRITE;
+/*!40000 ALTER TABLE `picture` DISABLE KEYS */;
+/*!40000 ALTER TABLE `picture` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -212,6 +316,29 @@ LOCK TABLES `sub_category` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `tags`
+--
+
+DROP TABLE IF EXISTS `tags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tags` (
+  `tag_id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`tag_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tags`
+--
+
+LOCK TABLES `tags` WRITE;
+/*!40000 ALTER TABLE `tags` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tags` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `users`
 --
 
@@ -220,12 +347,14 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `e_mail` varchar(45) NOT NULL,
   `name` varchar(45) NOT NULL,
   `age` int(11) NOT NULL,
-  `e_mail` varchar(45) NOT NULL,
   `avatar` blob,
   `role` int(11) NOT NULL,
-  PRIMARY KEY (`user_id`)
+  `is_registred` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  KEY `idx_users_name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='age<16 not allowed to see shocking news:)\nsuggest comments  has an user\nis_banned int?\nadd favorite_category?\nadd role : admin, regular, banned ?';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -237,6 +366,30 @@ LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `video`
+--
+
+DROP TABLE IF EXISTS `video`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `video` (
+  `video_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `content` blob NOT NULL,
+  PRIMARY KEY (`video_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `video`
+--
+
+LOCK TABLES `video` WRITE;
+/*!40000 ALTER TABLE `video` DISABLE KEYS */;
+/*!40000 ALTER TABLE `video` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -247,4 +400,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-10-07 12:27:08
+-- Dump completed on 2017-10-07 20:52:03
